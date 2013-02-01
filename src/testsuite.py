dc7b6908157953dfc330507928a95531d8b95a15
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2013-02-01 17:15:07 +0100

    testsuite.py: embed the test in a class
    The idea is to be able to run several tests in parallel. For that, we
    first need to embed each test in its own class, so that we can manupilate
    easier objects.

--- src/testsuite.py
+++ src/testsuite.py
@@ -38,21 +38,17 @@ hid_replay = hid_replay_path + '/' + hid_replay_cmd
 monitor = pyudev.Monitor.from_netlink(context)
 monitor.filter_by('input')
 
+global currentRunningHidTest
+currentRunningHidTest = None
 nodes = {}
-nodes_ready = []
-tests = []
-cv = threading.Condition()
-
-
-def reset():
-	global nodes, nodes_ready
-	nodes = {}
-	nodes_ready = []
 
 def log_event(action, device):
 	if 'event' in device.sys_name:
 #		print action, device, device.sys_name
 		if action == 'add':
+			if not currentRunningHidTest:
+				return
+
 			tmp = os.tmpfile()
 
 			# get node attributes
@@ -63,11 +59,12 @@ def log_event(action, device):
 			p = subprocess.Popen(shlex.split("evemu-record " + dev_path), stderr=None, stdout=tmp)
 
 			# store it for later
-			nodes[device.sys_name] = tmp, p
+			nodes[device.sys_name] = tmp, p, currentRunningHidTest
+			currentRunningHidTest.nodes[device.sys_name] = tmp, p, currentRunningHidTest
 
 		elif action == 'remove':
 			# get corresponding capturing process in background
-			result, p = nodes[device.sys_name]
+			result, p, hid = nodes[device.sys_name]
 
 			# wait for it to terminate
 			p.wait()
@@ -84,152 +81,169 @@ def log_event(action, device):
 			result.seek(0)
 
 			# notify test_hid that we are done with the capture of this node
-			cv.acquire()
-			nodes_ready.append((device.sys_name, name, result))
+			hid.cv.acquire()
+			hid.nodes_ready.append((device.sys_name, name, result))
 			del nodes[device.sys_name]
-			cv.notify()
-			cv.release()
+			del hid.nodes[device.sys_name]
+			hid.cv.notify()
+			hid.cv.release()
 
 
 observer = pyudev.MonitorObserver(monitor, log_event)
 observer.start()
 
-def dump_outs(file, outs):
-	hid_name = os.path.splitext(os.path.basename(file))[0]
-	for i in xrange(len(outs)):
-		out = outs[i]
-		out.seek(0)
-		ev_name = hid_name + '_' + str(i) + ".ev"
-		print ev_name
-		expected = open(ev_name, 'w')
-		for l in out.readlines():
-			expected.write(l)
+tests = []
 
-def dump_diff(name, events_file):
-	events_file.seek(0)
-	descr, frames = compare_evemu.parse_evemu(events_file)
-	output = open(name, 'w')
-	f_number = 0
-	for d in descr:
-		output.write(d)
-	for time, n, frame, extras in frames:
-		f_number += 1
-		output.write('frame '+str(f_number) + ':\n')
-		for i in xrange(len(frame)):
-			end = '\n'
-			if i in extras:
-				end = '*\n'
-			output.write('    '+ frame[i] + end)
-	output.close()
+class HIDTest(object):
+	def __init__(self, path):
+		self.path = path
+		self.reset()
 
-def dump_diffs(file, expected, outs):
-	hid_name = os.path.splitext(os.path.basename(file))[0]
-	for i in xrange(len(outs)):
-		ev_name = hid_name + '_res_' + str(i) + ".evd"
-		print ev_name
-		dump_diff(ev_name, outs[i])
-	if not expected:
-		return
-	for i in xrange(len(expected)):
-		ev_name = hid_name + '_exp_' + str(i) + ".evd"
-		print ev_name
-		expect = open(expected[i], 'r')
-		dump_diff(ev_name, expect)
-		expect.close()
+	def reset(self):
+		self.nodes = {}
+		self.nodes_ready = []
+		self.cv = threading.Condition()
+		self.outs = []
+		self.expected = []
 
-def compare_result(file, name, expected, outs):
-	warning = False
-	if expected == None:
-		return False, warning
+	def dump_outs(self):
+		hid_name = os.path.splitext(os.path.basename(self.path))[0]
+		for i in xrange(len(self.outs)):
+			out = self.outs[i]
+			out.seek(0)
+			ev_name = hid_name + '_' + str(i) + ".ev"
+			print ev_name
+			expected = open(ev_name, 'w')
+			for l in out.readlines():
+				expected.write(l)
 
-	if len(outs) != len(expected):
-		return False, warning
+	def dump_diff(self, name, events_file):
+		events_file.seek(0)
+		descr, frames = compare_evemu.parse_evemu(events_file)
+		output = open(name, 'w')
+		f_number = 0
+		for d in descr:
+			output.write(d)
+		for time, n, frame, extras in frames:
+			f_number += 1
+			output.write('frame '+str(f_number) + ':\n')
+			for i in xrange(len(frame)):
+				end = '\n'
+				if i in extras:
+					end = '*\n'
+				output.write('    '+ frame[i] + end)
+		output.close()
 
-	for i in xrange(len(outs)):
-		out = outs[i]
-		expect = open(expected[i], 'r')
-		r, w = compare_evemu.compare_files(expect, out)
-		expect.close()
-		if w:
-			warning = True
-		if not r:
-			return r, warning
+	def dump_diffs(self):
+		hid_name = os.path.splitext(os.path.basename(self.path))[0]
+		for i in xrange(len(self.outs)):
+			ev_name = hid_name + '_res_' + str(i) + ".evd"
+			print ev_name
+			self.dump_diff(ev_name, self.outs[i])
+		if not self.expected:
+			return
+		for i in xrange(len(self.expected)):
+			ev_name = hid_name + '_exp_' + str(i) + ".evd"
+			print ev_name
+			expect = open(self.expected[i], 'r')
+			self.dump_diff(ev_name, expect)
+			expect.close()
 
-	return True, warning
+	def compare_result(self):
+		warning = False
+		if self.expected == None:
+			return False, warning
 
-def test_hid(file):
-	reset()
-	results = None
-	rname = os.path.splitext(os.path.basename(file))[0]
-	for ev_file in ev_files:
-		if rname in ev_file:
-			if not results:
-				results = []
-			results.append(ev_file)
+		if len(self.outs) != len(self.expected):
+			return False, warning
 
-	# In case there are several files, keep the right one
-	if results:
-		kernel_release = os.uname()[2]
-		_results = {}
-		for r in results:
-			basename = os.path.basename(r)
-			rkernel_release = os.path.basename(os.path.dirname(r)).rstrip('.x')
-			if not rkernel_release.startswith('3.'):
-				rkernel_release = kernel_release
-			if kernel_release < rkernel_release:
-				# ignore dumps from earlier kernels
-				continue
-			if basename not in _results.keys():
-				_results[basename] = r
-			else:
-				current_kernel_release = os.path.basename(os.path.dirname(_results[basename])).rstrip('.x')
-				if current_kernel_release < rkernel_release:
-					# overwrite the file only if the kernel release is earlier
+		for i in xrange(len(self.outs)):
+			out = self.outs[i]
+			expect = open(self.expected[i], 'r')
+			r, w = compare_evemu.compare_files(expect, out)
+			expect.close()
+			if w:
+				warning = True
+			if not r:
+				return r, warning
+
+		return True, warning
+
+	def run(self):
+		self.reset()
+		results = None
+		rname = os.path.splitext(os.path.basename(self.path))[0]
+		for ev_file in ev_files:
+			if rname in ev_file:
+				if not results:
+					results = []
+				results.append(ev_file)
+
+		# In case there are several files, keep the right one
+		if results:
+			kernel_release = os.uname()[2]
+			_results = {}
+			for r in results:
+				basename = os.path.basename(r)
+				rkernel_release = os.path.basename(os.path.dirname(r)).rstrip('.x')
+				if not rkernel_release.startswith('3.'):
+					rkernel_release = kernel_release
+				if kernel_release < rkernel_release:
+					# ignore dumps from earlier kernels
+					continue
+				if basename not in _results.keys():
 					_results[basename] = r
-		results = _results.values()
-		results.sort()
+				else:
+					current_kernel_release = os.path.basename(os.path.dirname(_results[basename])).rstrip('.x')
+					if current_kernel_release < rkernel_release:
+						# overwrite the file only if the kernel release is earlier
+						_results[basename] = r
+			results = _results.values()
+			results.sort()
 
-	print "testing", file, "against", results
-	if subprocess.call(shlex.split(hid_replay + " -s 1 -1 " + file)):
-		return -1
+		self.expected = results
 
-	# wait for all the captures to finish
-	cv.acquire()
-	while len(nodes.keys()) > 0:
-		cv.wait()
-	cv.release()
+		print "testing", self.path, "against", results
+		if subprocess.call(shlex.split(hid_replay + " -s 1 -1 " + self.path)):
+			return -1
 
-	# retrieve the different captures
-	outs = []
-	for sys_name, name, out in nodes_ready:
-		outs.append(out)
+		# wait for all the captures to finish
+		self.cv.acquire()
+		while len(self.nodes.keys()) > 0:
+			self.cv.wait()
+		self.cv.release()
 
-	# compare them
-	r, w = compare_result(file, name, results, outs)
+		# retrieve the different captures
+		for sys_name, name, out in self.nodes_ready:
+			self.outs.append(out)
 
-	if not r:
-		# if there is a change, then dump the captures in the current directory
-		print "test failed, dumping outputs in:"
-		dump_outs(file, outs)
-		dump_diffs(file, results, outs)
-	elif w:
-		# if there is a warning, still dump the captures in the current directory
-		print "success but warning raised, dumping outputs in:"
-		dump_outs(file, outs)
-	else:
-		print "success"
+		# compare them
+		r, w = self.compare_result()
 
-	# close the captures so that the tmpfiles are destroyed
-	for out in outs:
-		out.close()
+		if not r:
+			# if there is a change, then dump the captures in the current directory
+			print "test failed, dumping outputs in:"
+			self.dump_outs()
+			self.dump_diffs()
+		elif w:
+			# if there is a warning, still dump the captures in the current directory
+			print "success but warning raised, dumping outputs in:"
+			self.dump_outs()
+		else:
+#			self.dump_outs()
+			print "success"
 
-	# append the result of the test to the list,
-	# we only count the warning if the test passed
-	tests.append((file, (r, w and r)))
+		# close the captures so that the tmpfiles are destroyed
+		for out in self.outs:
+			out.close()
 
-	print "----------------------------------------------------------------"
+		# append the result of the test to the list,
+		# we only count the warning if the test passed
+		tests.append((self.path, (r, w and r)))
 
-	return None
+		print "----------------------------------------------------------------"
+
+		return None
 
 def report_results(tests):
 	good = 0
@@ -269,11 +283,13 @@ try:
 	# if specific devices are given, treat them, otherwise, run the test on all .hid
 	if len(sys.argv) > 2:
 		for file in sys.argv[2:]:
-			test_hid(file)
+			currentRunningHidTest = HIDTest(file)
+			currentRunningHidTest.run()
 	else:
 		hid_files.sort()
 		for file in hid_files:
-			if test_hid(file):
+			currentRunningHidTest = HIDTest(file)
+			if currentRunningHidTest.run():
 				break
 finally:
 	report_results(tests)
